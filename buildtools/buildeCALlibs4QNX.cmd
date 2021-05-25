5fc4a8598807fe2f48c72e047a30c550a8571d0b
promero <promero@mathworks.com>
2021-05-25 18:07:56 +0200

    Added CMake error checks in build script.

--- buildtools/buildeCALlibs4QNX.cmd
+++ buildtools/buildeCALlibs4QNX.cmd
@@ -5,6 +5,8 @@ call %ALLUSERSPROFILE%"\MATLAB\SupportPackages\R2021a\toolbox\slrealtime\target\
 
 echo Creating makefiles for eCAL...
 cmake "..\modules\ecal" -B"../_build_qnx" -DCMAKE_TOOLCHAIN_FILE="..\buildtools\qnx.cmake" -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../_install_qnx -DHAS_HDF5=OFF -DHAS_QT5=OFF -DHAS_CURL=ON -DBUILD_APPS=OFF -DECAL_THIRDPARTY_BUILD_PROTOBUF=ON -Dprotobuf_BUILD_PROTOC_BINARIES=OFF -DECAL_THIRDPARTY_BUILD_FINEFTP=OFF -DECAL_THIRDPARTY_BUILD_CURL=OFF -DECAL_THIRDPARTY_BUILD_HDF5=OFF -Wno-dev
+if errorlevel 1 exit /b %ERRORLEVEL%
 
 echo Building libraries...
 cmake --build ../_build_qnx --target install
+if errorlevel 1 exit /b %ERRORLEVEL%
