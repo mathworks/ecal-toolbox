31c2aa41add2ac1a613abecfca08f87649e58133
promero <promero@mathworks.com>
2021-05-25 17:43:50 +0200

    Removed protoc modules and improved build process for QNX. Enabled support for ecaltime.

--- buildtools/qnx.cmake
+++ buildtools/qnx.cmake
@@ -15,12 +15,3 @@ set(CMAKE_CXX_FLAGS_INIT "-D_QNX_SOURCE -stdlib=libstdc++")
 
 # Explicitly link libsocket
 set(CMAKE_EXE_LINKER_FLAGS_INIT "-lsocket")
-
-# Set protoc properties for cross-compiling
-add_executable(protoc IMPORTED)
-if(CMAKE_HOST_SYSTEM_NAME STREQUAL "Windows")
-	set_target_properties(protoc PROPERTIES IMPORTED_LOCATION ${CMAKE_CURRENT_SOURCE_DIR}/../protoc-3.11.4-win64/bin/protoc.exe)
-else()
-	set_target_properties(protoc PROPERTIES IMPORTED_LOCATION ${CMAKE_CURRENT_SOURCE_DIR}/../protoc-3.11.4-linux-x86_64/bin/protoc)
-endif()
-set_target_properties(protoc PROPERTIES IMPORTED_GLOBAL TRUE)
