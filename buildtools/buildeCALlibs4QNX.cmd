e8c924a76f4958cd5adca08f592e9a11a23b1491
promero <promero@mathworks.com>
2021-05-25 11:54:43 +0200

    Updated build script for MATLAB R2021a.

--- buildtools/buildeCALlibs4QNX.cmd
+++ buildtools/buildeCALlibs4QNX.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 echo Creating QNX environment variables... 
-call %ALLUSERSPROFILE%"\MATLAB\SupportPackages\R2020b\toolbox\slrealtime\target\supportpackage\qnx700\qnxsdp-env.bat"
+call %ALLUSERSPROFILE%"\MATLAB\SupportPackages\R2021a\toolbox\slrealtime\target\supportpackage\qnx710\qnxsdp-env.bat"
 
 echo Creating makefiles for eCAL...
 cmake "..\modules\ecal" -B"../_build_qnx" -DCMAKE_TOOLCHAIN_FILE="..\buildtools\qnx.cmake" -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../_install_qnx -DHAS_HDF5=OFF -DHAS_QT5=OFF -DHAS_CURL=ON -DBUILD_APPS=OFF -DBUILD_TIME=OFF -DECAL_THIRDPARTY_BUILD_PROTOBUF=ON -Dprotobuf_BUILD_PROTOC_BINARIES=OFF -DECAL_THIRDPARTY_BUILD_FINEFTP=OFF -DECAL_THIRDPARTY_BUILD_CURL=OFF -DECAL_THIRDPARTY_BUILD_HDF5=OFF -Wno-dev
