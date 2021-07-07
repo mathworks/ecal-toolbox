dd0506bd6073f4b1615d417d032f0d350141c65a
promero <promero@mathworks.com>
2021-07-07 12:21:18 +0200

    Fixed setting model name as eCAL unit name. Display eCAL version at initialization. Warning if two eCAL models running in the same MATLAB instance.

--- src/s_ecal_common.cpp
+++ src/s_ecal_common.cpp
@@ -12,9 +12,9 @@
 namespace s_eCAL {
 
 void Initialize (SimStruct *S) {
-        if(!eCAL::IsInitialized())
-        {
-            if(eCAL::Initialize()) {
+    const std::string model_path(ssGetModelName(ssGetRootSS(S)));
+        if(!eCAL::IsInitialized()) {
+            if(eCAL::Initialize(0, nullptr, model_path.c_str())) {
                 #ifdef SIMULINK_REAL_TIME
                 LOG(error,0) << "Error initializing eCAL";
                 exit(EXIT_FAILURE);
@@ -22,14 +22,21 @@ void Initialize (SimStruct *S) {
                 ssSetLocalErrorStatus(S, "Error initializing eCAL");
                 #endif
             } else {
+                static char startmsg[100];
+                sprintf(startmsg,"eCAL %s initialized\n",eCAL::GetVersionString());
                 #ifdef SIMULINK_REAL_TIME
-                LOG(info,0) << "eCAL initialized";
+                LOG(info,0) << startmsg;
                 #else
-                ssPrintf("eCAL initialized");
+                ssPrintf(startmsg);
                 #endif
             }
-            const std::string model_path(ssGetModelName(S)); 
-            eCAL::SetUnitName(model_path.c_str());
+        } else {
+            const std::string ecalUnitName(eCAL::Process::GetUnitName());
+            if(ecalUnitName != model_path) {
+                static char warningmsg[300];
+                sprintf(warningmsg,"eCAL has already been initialized at %s. Please start another MATLAB instance to use eCAL on %s.",ecalUnitName.c_str(),model_path.c_str());
+                ssWarning(S, warningmsg);
+            }
         }
 }
 
