b06372bcd7b623982a51a98837b7b7ec84f88847
promero <promero@mathworks.com>
2021-08-25 19:12:15 +0200

    Set some build settings to OFF to match QNX build.

--- buildtools/buildeCALlibs4Linux.sh
+++ buildtools/buildeCALlibs4Linux.sh
@@ -4,7 +4,7 @@
 cd "$(dirname "$0")"
 
 echo Creating makefiles...
-cmake "..\modules\ecal" -B"../_build_linux" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../_install_linux -DBUILD_APPS=OFF -DECAL_THIRDPARTY_BUILD_FINEFTP=OFF -DECAL_THIRDPARTY_BUILD_CURL=OFF -DECAL_THIRDPARTY_BUILD_HDF5=OFF -Wno-dev
+cmake "..\modules\ecal" -B"../_build_linux" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../_install_linux -DHAS_HDF5=OFF -DHAS_QT5=OFF -DBUILD_APPS=OFF -DECAL_THIRDPARTY_BUILD_PROTOBUF=ON -DECAL_THIRDPARTY_BUILD_FINEFTP=OFF -DECAL_THIRDPARTY_BUILD_CURL=OFF -DECAL_THIRDPARTY_BUILD_HDF5=OFF -Wno-dev
 
 echo Building libraries...
 cmake --build "../_build_linux" --target install
