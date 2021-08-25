6165b29d63437f2aa81f5a5f25453d020ea57f0b
promero <promero@mathworks.com>
2021-08-25 18:35:36 +0200

    Add build script to build eCAL on Linux

---
+++ buildtools/buildeCALlibs4Linux.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Change directory to the directory where this file is
+cd "$(dirname "$0")"
+
+echo Creating makefiles...
+cmake "..\modules\ecal" -B"../_build_linux" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../_install_linux -DBUILD_APPS=OFF -DECAL_THIRDPARTY_BUILD_FINEFTP=OFF -DECAL_THIRDPARTY_BUILD_CURL=OFF -DECAL_THIRDPARTY_BUILD_HDF5=OFF -Wno-dev
+
+echo Building libraries...
+cmake --build "../_build_linux" --target install
