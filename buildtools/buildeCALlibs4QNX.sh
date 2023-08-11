ee903f179a6119e43d3b15a38b938a9e9e02778e
Pablo Romero <promero@mathworks.com>
2023-08-11 09:47:55 +0200

    Implements #5.

---
+++ buildtools/buildeCALlibs4QNX.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Change directory to the directory where this file is
+cd "$(dirname "$0")"
+
+# Define MATLAB version
+MATLABVER=R2021a
+
+echo Creating QNX environment variables... 
+source /home/$USER/Documents/MATLAB/SupportPackages/$MATLABVER/toolbox/slrealtime/target/supportpackage/qnx710/qnxsdp-env.sh
+
+echo Creating makefiles...
+cmake "..\modules\ecal" -B"../_build_qnx" -DCMAKE_TOOLCHAIN_FILE="..\buildtools\qnx.cmake" -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../_install_qnx -DHAS_HDF5=OFF -DHAS_QT5=OFF -DHAS_CURL=ON -DBUILD_APPS=OFF -DECAL_THIRDPARTY_BUILD_PROTOBUF=ON -Dprotobuf_BUILD_PROTOC_BINARIES=OFF -DECAL_THIRDPARTY_BUILD_FINEFTP=OFF -DECAL_THIRDPARTY_BUILD_CURL=OFF -DECAL_THIRDPARTY_BUILD_HDF5=OFF -Wno-dev
+
+echo Building libraries...
+cmake --build "../_build_qnx" --target install
